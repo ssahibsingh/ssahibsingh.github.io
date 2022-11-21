350067625066416deee5f3ea69e0b36a43f95453
Sahib Singh <75523820+ssahibsingh@users.noreply.github.com>
2022-11-21 19:37:22 +0530

    updated other project link to personal link

--- data/project.js
+++ data/project.js
@@ -66,67 +66,67 @@ const otherProjects = [
     {
         key: 1,
         link: "https://github.com/ggeeks/ggeeks.github.io",
-        img: "https://github-readme-stats.vercel.app/api/pin/?username=ggeeks&repo=ggeeks.github.io&show_owner=true&border_color=fafafa&bg_color=fafafa&border_radius=10"
+        img: "https://github-readme-stats-ssahibsingh.vercel.app/api/pin/?username=ggeeks&repo=ggeeks.github.io&show_owner=true&border_color=fafafa&bg_color=fafafa&border_radius=10"
     },
     {
         key: 8,
         link: "https://github.com/ssahibsingh/blog-website-pub",
-        img: "https://github-readme-stats.vercel.app/api/pin/?username=ssahibsingh&repo=blog-website-pub&bg_color=fafafa&border_color=fafafa&border_radius=10"
+        img: "https://github-readme-stats-ssahibsingh.vercel.app/api/pin/?username=ssahibsingh&repo=blog-website-pub&bg_color=fafafa&border_color=fafafa&border_radius=10"
     },
     {
         key: 9,
         link: "https://github.com/ssahibsingh/qrcode-generator",
-        img: "https://github-readme-stats.vercel.app/api/pin/?username=ssahibsingh&repo=qrcode-generator&bg_color=fafafa&border_color=fafafa&border_radius=10"
+        img: "https://github-readme-stats-ssahibsingh.vercel.app/api/pin/?username=ssahibsingh&repo=qrcode-generator&bg_color=fafafa&border_color=fafafa&border_radius=10"
     },
     {
         key: 10,
         link: "https://github.com/ggeeks/event-ggeeks",
-        img: "https://github-readme-stats.vercel.app/api/pin/?username=ggeeks&repo=event-ggeeks&show_owner=true&border_color=fafafa&bg_color=fafafa&border_radius=10"
+        img: "https://github-readme-stats-ssahibsingh.vercel.app/api/pin/?username=ggeeks&repo=event-ggeeks&show_owner=true&border_color=fafafa&bg_color=fafafa&border_radius=10"
     },
     {
         key: 11,
         link: "https://github.com/ssahibsingh/Project-GamingGeeks",
-        img: "https://github-readme-stats.vercel.app/api/pin/?username=ssahibsingh&repo=Project-GamingGeeks&bg_color=fafafa&border_color=fafafa&border_radius=10"
+        img: "https://github-readme-stats-ssahibsingh.vercel.app/api/pin/?username=ssahibsingh&repo=Project-GamingGeeks&bg_color=fafafa&border_color=fafafa&border_radius=10"
     },
     {
         key: 12,
         link: "https://github.com/ssahibsingh/Project-TiwanaPowerSol",
-        img: "https://github-readme-stats.vercel.app/api/pin/?username=ssahibsingh&repo=Project-TiwanaPowerSol&bg_color=fafafa&border_color=fafafa&border_radius=10"
+        img: "https://github-readme-stats-ssahibsingh.vercel.app/api/pin/?username=ssahibsingh&repo=Project-TiwanaPowerSol&bg_color=fafafa&border_color=fafafa&border_radius=10"
     },
     {
         key: 13,
         link: "https://github.com/ssahibsingh/todolist-pub",
-        img: "https://github-readme-stats.vercel.app/api/pin/?username=ssahibsingh&repo=todolist-pub&bg_color=fafafa&border_color=fafafa&border_radius=10"
+        img: "https://github-readme-stats-ssahibsingh.vercel.app/api/pin/?username=ssahibsingh&repo=todolist-pub&bg_color=fafafa&border_color=fafafa&border_radius=10"
     },
     {
         key: 14,
         link: "https://github.com/ssahibsingh/newsletter-signup-pub",
-        img: "https://github-readme-stats.vercel.app/api/pin/?username=ssahibsingh&repo=newsletter-signup-pub&bg_color=fafafa&border_color=fafafa&border_radius=10"
+        img: "https://github-readme-stats-ssahibsingh.vercel.app/api/pin/?username=ssahibsingh&repo=newsletter-signup-pub&bg_color=fafafa&border_color=fafafa&border_radius=10"
     },
     {
         key: 15,
         link: "https://github.com/ssahibsingh/TossTheDice",
-        img: "https://github-readme-stats.vercel.app/api/pin/?username=ssahibsingh&repo=TossTheDice&bg_color=fafafa&border_color=fafafa&border_radius=10"
+        img: "https://github-readme-stats-ssahibsingh.vercel.app/api/pin/?username=ssahibsingh&repo=TossTheDice&bg_color=fafafa&border_color=fafafa&border_radius=10"
     },
     {
         key: 16,
         link: "https://github.com/ssahibsingh/simongame",
-        img: "https://github-readme-stats.vercel.app/api/pin/?username=ssahibsingh&repo=simongame&bg_color=fafafa&border_color=fafafa&border_radius=10"
+        img: "https://github-readme-stats-ssahibsingh.vercel.app/api/pin/?username=ssahibsingh&repo=simongame&bg_color=fafafa&border_color=fafafa&border_radius=10"
     },
     {
         key: 17,
         link: "https://github.com/ssahibsingh/drumkit",
-        img: "https://github-readme-stats.vercel.app/api/pin/?username=ssahibsingh&repo=drumkit&bg_color=fafafa&border_color=fafafa&border_radius=10"
+        img: "https://github-readme-stats-ssahibsingh.vercel.app/api/pin/?username=ssahibsingh&repo=drumkit&bg_color=fafafa&border_color=fafafa&border_radius=10"
     },
     {
         key: 18,
         link: "https://github.com/ssahibsingh/flipkartclone",
-        img: "https://github-readme-stats.vercel.app/api/pin/?username=ssahibsingh&repo=flipkartclone&bg_color=fafafa&border_color=fafafa&border_radius=10"
+        img: "https://github-readme-stats-ssahibsingh.vercel.app/api/pin/?username=ssahibsingh&repo=flipkartclone&bg_color=fafafa&border_color=fafafa&border_radius=10"
     },
     {
         key: 19,
         link: "https://github.com/ssahibsingh/chaining-the-EVMs",
-        img: "https://github-readme-stats.vercel.app/api/pin/?username=ssahibsingh&repo=chaining-the-EVMs&bg_color=fafafa&border_color=fafafa&border_radius=10"
+        img: "https://github-readme-stats-ssahibsingh.vercel.app/api/pin/?username=ssahibsingh&repo=chaining-the-EVMs&bg_color=fafafa&border_color=fafafa&border_radius=10"
     },
 ]
 
