ecf18270cf198a6547770644f86c55ee0c7a727b
Sahib Singh <75523820+ssahibsingh@users.noreply.github.com>
2023-01-23 09:25:50 +0530

    updated hello api route

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(
+    {
+      name: 'Sahib Singh',
+      about: "Full Stack Web Developer || Co-Founder Gaming Geeks || IT Undergrad at NIT Jalandhar",
+      resume: "https://ssahibsingh.github.io/resume",
+      contact: "https://ssahibsingh.github.io/#contact",
+    })
 }
