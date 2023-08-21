086e925130774ea528bf43acc5e61f87248b2a8a
ssahibsingh <snone181@gmail.com>
2023-08-21 23:09:11 +0530

    updated

--- pages/index.js
+++ pages/index.js
@@ -28,8 +28,17 @@ export default function Home() {
         <meta property="twitter:title" content="Sahib Singh" />
         <meta property="twitter:description" content="Web Developer || Co-Founder at Gaming Geeks || IT undergrad at NITJ" />
         <meta property="twitter:image" content="./website.png" />
-          
-        <meta name="google-site-verification" content="mzoNw9Qn19gAfDXgZGNJIw4aOhU46Gw2kLTcZPgfH1s" />  
+
+        <meta name="google-site-verification" content="mzoNw9Qn19gAfDXgZGNJIw4aOhU46Gw2kLTcZPgfH1s" />
+
+        <script>
+
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5GZZMV3R');`}}</script>
       </Head>
 
       <Navbar />
@@ -40,7 +49,8 @@ export default function Home() {
       <Education />
       <Contact />
       <Footer />
-
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5GZZMV3R"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     </>
   )
 }
