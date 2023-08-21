0028a202041f82b6086d58a25a1639101c59b7c5
ssahibsingh <snone181@gmail.com>
2023-08-21 11:54:36 +0530

    updated resume

--- pages/index.js
+++ pages/index.js
@@ -11,20 +11,20 @@ export default function Home() {
         <title>Sahib Singh</title>
         <meta name="title" content="Sahib Singh" />
         <meta name="author" content="Sahib Singh" />
-        <meta name="description" content="Web Developer || Co-Founder at Gaming Geeks || IT undergrad at NITJ" />
+        <meta name="description" content="Full Stack Web Developer || Co-Founder at Gaming Geeks || IT undergrad at NITJ" />
         <meta name="keywords"
           content="Sahib Singh, Sahib, Soorme, Sardar Sahib Singh, Sahib Singh Khanna, Sahib Singh Github, Sahib Singh LinkedIn, Sahib Singh Gaming Geeks, ssahibsingh, Gaming Geeks, Soorme.com, Giigaa.com " />
         {/* <link rel="canonical" href="" /> */}
         {/* Open Graph / Facebook */}
         <meta property="og:type" content="website" />
-        {/* <meta property="og:url" content="https://sahibsingh.vercel.app" /> */}
+        <meta property="og:url" content="https://sahibsingh.dev" />
         <meta property="og:title" content="Sahib Singh" />
         <meta property="og:description" content="Web Developer || Co-Founder at Gaming Geeks || IT undergrad at NITJ" />
         <meta property="og:image" content="./website.png" />
 
         {/* Twitter  */}
         <meta property="twitter:card" content="summary_large_image" />
-        {/* <meta property="twitter:url" content="https://sahibsingh.vercel.app" /> */}
+        <meta property="twitter:url" content="https://sahibsingh.dev" />
         <meta property="twitter:title" content="Sahib Singh" />
         <meta property="twitter:description" content="Web Developer || Co-Founder at Gaming Geeks || IT undergrad at NITJ" />
         <meta property="twitter:image" content="./website.png" />
