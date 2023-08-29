5f9bd0069ec9234622d31cff65c5f202e887aa39
ssahibsingh <snone181@gmail.com>
2023-08-29 20:29:08 +0530

    updated

--- pages/index.js
+++ pages/index.js
@@ -30,15 +30,6 @@ export default function Home() {
         <meta property="twitter:image" content="./website.png" />
 
         <meta name="google-site-verification" content="mzoNw9Qn19gAfDXgZGNJIw4aOhU46Gw2kLTcZPgfH1s" />
-
-        <script>
-
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5GZZMV3R');`}}</script>
       </Head>
 
       <Navbar />
@@ -49,8 +40,6 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       <Education />
       <Contact />
       <Footer />
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5GZZMV3R"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     </>
   )
 }
